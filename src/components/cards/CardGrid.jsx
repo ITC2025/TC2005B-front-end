cf1d5769f0789bdf3d875b64c7b01255c88864d3
NovoxMX <A00835132@tec.mx>
2023-04-14 18:38:38 -0700

    feat: add bootstrap to CardGrid

--- src/components/cards/CardGrid.jsx
+++ src/components/cards/CardGrid.jsx
@@ -3,14 +3,31 @@ import CardRequest from "./../../images/CardRequest.png";
 import CardProject from "./../../images/CardProject.png";
 import CardTravelAll from './../../images/CardTravelAll.png'
 
+
 function CardGrid() {
   return (
     <>
-      <CardDashboard image={CardProject}></CardDashboard>
-      <CardDashboard image={CardRequest}></CardDashboard>
-      <CardDashboard image={CardTravelAll}></CardDashboard>
-    </>
-  );
+
+      <div className="container text-center">
+        <div className = "my-5"></div>
+        <div className="row justify-content-center">
+          <div className="col-6">
+            <CardDashboard image={CardProject}></CardDashboard>
+          </div>
+          <div className="col-6">
+            <CardDashboard image={CardRequest}></CardDashboard>
+          </div>
+
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-6">
+          <CardDashboard image={CardTravelAll}></CardDashboard>
+          </div>
+        </div>
+      </div>
+      </>
+      );
 }
 
-export default CardGrid;
+      export default CardGrid;
