f077dcb1ddc91b4e5f582af0c7e780ea52e88dfc
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-16 16:14:18 -0700

    fix: delete extra lines

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import './App.css';
-import pmDashboard from "./pages/Dashboard/pmDashboard.js";
-import CardDashboard from "./components/cards/CardDashboard";
-import CardGrid from "./components/cards/CardGrid";
 import { TableTravelAllowance } from "./components/table/TableTravelAllowance";
 
 function App() {
