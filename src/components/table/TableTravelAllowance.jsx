8aa39afc6516c80dfb93d86ee76f6dca0ac9b03d
NovoxMX <A00835132@tec.mx>
2023-04-16 19:54:41 -0700

    feat: Added text filter, and sort colums

--- src/components/table/TableTravelAllowance.jsx
+++ src/components/table/TableTravelAllowance.jsx
@@ -8,6 +8,7 @@ export const TableTravelAllowance = () => {
 
   // Funcion para mostrar datos con fetch
   const URL = 'https://gorest.co.in/public/v2/users'
+
   const getTravelAllowance = async () => {
     const res = await fetch(URL)
     const data = await res.json()
@@ -15,48 +16,70 @@ export const TableTravelAllowance = () => {
     console.log(data)
     
   }
+
   useEffect(() => {
     getTravelAllowance()
   }
   , [])
 
+  // Funcion para filtrar datos
+  const handleFilter = (e) => {
+    const searchWord = e.target.value
+    const newFilter = travelAllowance.filter((value) => {
+      return value.name.toLowerCase().includes(searchWord.toLowerCase())
+    })
+    setTravelAllowance(newFilter)
+  }
+
+    
   // configuracion de columnas
   const columns = [
     {
       name: 'ID',
-      selector: row => row.id
+      selector: row => row.id,
+      sortable: true
     },
     {
       name: 'NAME',
-      selector: row => row.name
+      selector: row => row.name,
+      sortable: true
     },
     {
       name: 'Project',
-      selector: row => row.email
+      selector: row => row.email,
+      sortable: true
     },
     // {
     //   name: 'Description',
-    //   selector: row => row.gender
+    //   selector: row => row.gender,
+    //   sortable: true
     // },
     // {
     //   name: 'Total',
-    //   selector: row => row.status
+    //   selector: row => row.status,
+    //   sortable: true
     // },
     // {
     //   name: 'Status',
-    //   selector: row => row.status
+    //   selector: row => row.status,
+    //   sortable: true
     // },
     // {
     //   name: 'Actions',
-      
+    //   
+    //   sortable: true
     // }
   ]
   // mostrar la tabla
   return (
-    <DataTable
+    <div className='Container text-center'>
+      <div className='text-end'> <input type="text" onChange={handleFilter}/></div>
+      <DataTable
       columns={columns} 
       data={travelAllowance}
       pagination
+      fixedHeader
       />
+    </div>
   )
 }
