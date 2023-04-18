ecb84810b94a1f582adc2f2087f74db73600c6e2
Kmaldon <A01252830@itesm.mx>
2023-04-18 03:56:58 -0700

    Update Form

--- src/pages/Gastos/gastos.js
+++ src/pages/Gastos/gastos.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Formgastos from "../../components/Formgastos/index";
+import AddedForm from "../../components/ButtonForm/index";
 import {
     Col,
     Button,
     Row,
     Container,
     Card,
-    Form,
-    InputGroup,
+    FormFloating,
+    Stack,
 } from "react-bootstrap";
-import { MdCalendarMonth, MdLocationPin, MdExpandMore, MdOutlineFileUpload } from "react-icons/md";
 import { HiPlus } from "react-icons/hi";
 import { BiMoney } from "react-icons/bi";
 
@@ -17,27 +17,37 @@ import { BiMoney } from "react-icons/bi";
 import '../../styles/gastos.css';
 
 export default function Gastos() {
+    const [forms, setForms] = useState([<AddedForm key={0} />]);
+
+    const addForm = () =>{
+        const newForms = [...forms, <AddedForm key={forms.length} />];
+        setForms(newForms);
+    };
+
     return (
         <>
-        <h2 id="gastos">Gastos</h2>
         <Container>
-            <Row className="vh-100 align-items-center">
+        <h2 id="gastos">Gastos</h2>
+            <Row>
                 <Col>
                     <Card id="container1">
-
-                        <Formgastos />
-
-                        <div className="d-grid mt-4">
-                            <Button variant="danger" type="submit" id="button">
-                                <BiMoney />
-                                AGREGAR GASTO
-                                <HiPlus />
-                            </Button>
-                        </div>
+                        <Container>
+                                {forms}
+                                <div className="d-flex justify-content-center">
+                                    <Button variant="danger" id="button" onClick={addForm}>
+                                        AGREGAR GASTO
+                                    </Button>
+                                </div>
+                        </Container>
                     </Card>
                 </Col>
             </Row>
         </Container>
+        <div className="d-flex justify-content-end align-items-end">
+            <Button variant="danger" id="CGbutton">CANCELAR</Button>
+            <Button variant="danger" id="CGbutton">GUARDAR</Button>
+        </div>
         </>
     );
 }  
+
