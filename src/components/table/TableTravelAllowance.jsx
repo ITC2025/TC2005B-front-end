3e3413ac2d82689274a96d89084ec14faf0f489f
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-17 17:15:32 -0700

    feat: add tabel styles

--- src/components/table/TableTravelAllowance.jsx
+++ src/components/table/TableTravelAllowance.jsx
@@ -1,50 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import DataTable from 'react-data-table-component'
-import TableStyle from '../../styles/TableStyle.css'
+import React, { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+import "../../styles/TableStyle.css";
 
 export const TableTravelAllowance = () => {
   // Configurar hooks
-  const [travelAllowance, setTravelAllowance] = useState([])
-  const [filtertravelAllowance, setFilterTravelAllowance] = useState([])
+  const [travelAllowance, setTravelAllowance] = useState([]);
+  const [filtertravelAllowance, setFilterTravelAllowance] = useState([]);
 
   // Funcion para mostrar datos con fetch
-  const URL = 'https://gorest.co.in/public/v2/users'
+  // const URL = 'https://gorest.co.in/public/v2/posts'
+  const URL = "https://jsonplaceholder.typicode.com/users";
 
   const getTravelAllowance = async () => {
-    const res = await fetch(URL)
-    const data = await res.json()
-    setTravelAllowance(data)
-    setFilterTravelAllowance(data)
-    console.log(data)
-  }
+    const res = await fetch(URL);
+    const data = await res.json();
+    setTravelAllowance(data);
+    setFilterTravelAllowance(data);
+    console.log(data);
+  };
 
   useEffect(() => {
-    getTravelAllowance()
-  }
-  , [])
+    getTravelAllowance();
+  }, []);
 
   // Funcion para filtrar datos
   const handleFilter = (e) => {
-    const newData = filtertravelAllowance.filter(row => row.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    const newData = filtertravelAllowance.filter((row) =>
+      row.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
     setTravelAllowance(newData);
-  }
-    
+  };
+
   // configuracion de columnas
   const columns = [
     {
-      name: 'ID',
-      selector: row => row.id,
-      sortable: true
+      name: "ID",
+      selector: (row) => row.id,
+      sortable: true,
     },
     {
-      name: 'NAME',
-      selector: row => row.name,
-      sortable: true
+      name: "NAME",
+      selector: (row) => row.name,
+      sortable: true,
     },
     {
-      name: 'Project',
-      selector: row => row.email,
-      sortable: true
+      name: "Project",
+      selector: (row) => row.email,
+      sortable: true,
     },
     // {
     //   name: 'Description',
@@ -63,24 +65,33 @@ export const TableTravelAllowance = () => {
     // },
     // {
     //   name: 'Actions',
-    //   
-    //   sortable: true
+    //
+    //
     // }
-  ]
+  ];
+
+  const customStyles = {
+    rowsPerPageNext: "Filas por pagina",
+    rageSeparatorText: "de",
+    selectAllRowsItem: true,
+    selectAllRowsItemText: "Todos",
+  }
 
   // mostrar la tabla
   return (
-    <div className='Container text-center'>
-      <div className='text-end'> 
-      <input type="text" placeholder='Search' onChange={handleFilter}/>
+    <div className="Container text-center">
+      <div className="text-end my-1">
+        <input type="text" placeholder="Search" onChange={handleFilter} />
       </div>
 
       <DataTable
-      columns={columns} 
-      data={travelAllowance}
-      pagination
-      fixedHeader
+        columns={columns}
+        data={travelAllowance}
+        title="Mis Viaticos"
+        pagination
+        paginationComponentOptions={customStyles}
+        fixedHeader
       />
     </div>
-  )
-}
+  );
+};
