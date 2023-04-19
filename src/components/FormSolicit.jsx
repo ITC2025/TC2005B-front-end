3229366dcda1baf70893d9f3982ae1a112d50179
Ximena <ximenaooflopez@gmail.com>
2023-04-18 22:15:19 -0700

    Fix: Bugs on FormSolicit

--- src/components/FormSolicit.jsx
+++ src/components/FormSolicit.jsx
@@ -2,50 +2,35 @@ import FormInputIcon from './FormInputIcon';
 import AddInputButton from './AddInputButton';
 import AddedInput from './AddedInput';
 import { Container, Row, Col } from 'react-bootstrap';
-import { useState } from "react";
 import '../styles/FormSolicit.css';
 
-function FormSolicit(props) {
-    const [FechaInicio, setFechaInicio] = useState('');
-    const [FechaTermino, setFechaTermino] = useState('');
-    const [Destino, setDestino] = useState('');
-    const [Proyecto, setProyecto] = useState('')
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        props.showModal(FechaInicio, FechaTermino, Destino, Proyecto);
-    };
+function FormSolicit() {
 
     return (
         <>
-            <Form onSubmit={handleSubmit}>
-                <Container id="FormSolicitComponent">
-                    <Row id="SolicitFormRow">
-                        <Col sm={10} md={5}>
-                            <FormInputIcon inputControlID="FechaInicio" inputNombre="Fecha Inicio" inputPlaceHolder=""
-                                inputType="date" value={FechaInicio} onChange={(event) => setFechaInicio(event.target.value)} />
-                        </Col>
-                        <Col sm={10} md={5}>
-                            <FormInputIcon inputControlID="FechaTermino" inputNombre="Fecha Termino" inputPlaceHolder=""
-                                inputType="date" value={FechaTermino} onChange={(event) => setFechaTermino(event.target.value)} />
-                        </Col>
-                    </Row>
-                    <Row id="SolicitFormRow">
-                        <Col sm={10} md={5}>
-                            <FormInputIcon inputControlID="Destino" inputNombre="Destino" inputPlaceHolder=""
-                                type="text" value={Destino} onChange={(event) => setDestino(event.target.value)} />
-                        </Col>
-                        <Col sm={10} md={5}>
-                            <FormInputIcon inputControlID="Proyecto" inputNombre="Proyecto" inputPlaceHolder=""
-                                type="text" value={Proyecto} onChange={(event) => setProyecto(event.target.value)} />
-                        </Col>
-                    </Row>
-                    <Row id="SolicitFormRow">
-                        <Col sm={10} md={10}>
-                            <AddInputButton InputComponent={AddedInput} />
-                        </Col>
-                    </Row>
-                </Container>
-            </Form>
+            <Container id="FormSolicitComponent">
+                <Row id="SolicitFormRow">
+                    <Col sm={10} md={5}>
+                        <FormInputIcon inputControlID="FechaInicio" inputNombre="Fecha Inicio" inputPlaceHolder="" inputType="date"  />
+                    </Col>
+                    <Col sm={10} md={5}>
+                        <FormInputIcon inputControlID="FechaTermino" inputNombre="Fecha Termino" inputPlaceHolder="" inputType="date" />
+                    </Col>
+                </Row>
+                <Row id="SolicitFormRow">
+                    <Col sm={10} md={5}>
+                        <FormInputIcon inputControlID="Destino" inputNombre="Destino" inputPlaceHolder="" type="text" />
+                    </Col>
+                    <Col sm={10} md={5}>
+                        <FormInputIcon inputControlID="Proyecto" inputNombre="Proyecto" inputPlaceHolder="" type="text" />
+                    </Col>
+                </Row>
+                <Row id="SolicitFormRow">
+                    <Col sm={10} md={10}>
+                        <AddInputButton InputComponent={AddedInput} />
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
