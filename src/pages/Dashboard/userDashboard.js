080e7bac4f770aba43431384dc5d7922dcce896d
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-19 22:21:16 -0700

    feat: add user's dashboard page

---
+++ src/pages/Dashboard/userDashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CardGridUser from "../../components/cards/CardGridUser";
+
+export const UserDashboard = () => {
+  return (
+    <CardGridUser
+      linkRequest="https://t4.ftcdn.net/jpg/02/36/57/91/360_F_236579161_BwYfCPuAytiZRCImG6yIe5RDBUlYpLCq.jpg"
+      linkTravelAll="https://mayaonmoney.co.za/wp-content/uploads/2021/03/travel-allowance.jpg"
+    />
+  );
+};
