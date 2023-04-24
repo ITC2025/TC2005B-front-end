be78589f25ee64602cced30c529c20e4507952bb
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-23 23:46:48 -0700

    feat: add Pm Table Dropdown

--- src/components/table/PmTableDropdown.jsx
+++ src/components/table/PmTableDropdown.jsx
@@ -1,20 +1,42 @@
-import Dropdown from 'react-bootstrap/Dropdown';
-import { MdOutlineMoreVert} from 'react-icons/md';
-import "../../styles/TableBadges.css"
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { MdOutlineMoreVert } from "react-icons/md";
+import "../../styles/TableBadges.css";
 
-function PmTableDropdown() {
+export default function PmTableDropdown() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <Dropdown>
-      <Dropdown.Toggle >
-        <MdOutlineMoreVert id='icon'  />
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Ver</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <MdOutlineMoreVert id="icon" />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Ver solicitud</MenuItem>
+      </Menu>
+    </div>
   );
 }
-
-export default PmTableDropdown;
