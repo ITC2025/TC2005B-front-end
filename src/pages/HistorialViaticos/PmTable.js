7b952531c234092f120d250c0573ad8842e70152
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-23 22:07:52 -0700

    feat: add pm table page

---
+++ src/pages/HistorialViaticos/PmTable.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+import { PmTableTravelAll } from '../../components/table/PmTableTravelAll'
+
+export const PmTable = () => {
+  return (
+    <div>
+        
+        <PmTableTravelAll/>
+    </div>
+  )
+}
