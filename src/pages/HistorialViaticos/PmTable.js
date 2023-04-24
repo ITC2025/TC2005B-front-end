b6ab9be3d9772316d4f57436d2ec56fb956dbc0f
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-24 11:59:16 -0700

    feat: add balance to main page

--- src/pages/HistorialViaticos/PmTable.js
+++ src/pages/HistorialViaticos/PmTable.js
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from "react";
+import "../../styles/PmTable.css";
 
-import { PmTableTravelAll } from '../../components/table/PmTableTravelAll'
+import { PmTableTravelAll } from "../../components/table/PmTableTravelAll";
 
 export const PmTable = () => {
+  const [datosCliente, setDatosCliente] = useState(null);
+
+  useEffect(() => {
+    fetch("https://gorest.co.in/public/v2/users?page=1&per_page=1")
+      .then((response) => response.json())
+      .then((data) => {
+        setDatosCliente(data);
+        console.log(data);
+      });
+  }, []);
+
+  if (!datosCliente) {
+    return <div>Cargando...</div>;
+  }
+
+
+
   return (
-    <div>
-        
-        <PmTableTravelAll/>
+    <div className="p-5">
+      <div className="row d-flex justify-content-end">
+        <div>SALDO DISPONIBLE: {datosCliente.id}</div>
+      </div>
+
+      <PmTableTravelAll />
     </div>
-  )
-}
+  );
+};
