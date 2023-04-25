0f9d684db6f2459106efe5d1a50ea3e29db032c0
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-24 19:54:30 -0700

    test: add userTable for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { PmTable } from "./pages/HistorialViaticos/PmTable";
+import { UserTable } from "./pages/HistorialViaticos/UserTable";
 import './App.css';
 
 function App() {
   return (
-    <PmTable/>
+    <UserTable  />
   );
 }
 
