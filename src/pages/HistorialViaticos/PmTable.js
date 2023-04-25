c2f388baf5b5191295dc26a9c419b2efacb84782
NovoxMx <A00835132@tec.mx>
2023-04-24 18:53:34 -0700

    fix: delete balance

--- src/pages/HistorialViaticos/PmTable.js
+++ src/pages/HistorialViaticos/PmTable.js
@@ -4,21 +4,7 @@ import "../../styles/PmTable.css";
 import { PmTableTravelAll } from "../../components/table/PmTableTravelAll";
 
 export const PmTable = () => {
-  const [cliente, setCliente] = useState([]);
-  const URL = "https://gorest.co.in/public/v2/users?page=1&per_page=1";
-  async function obtenerDatosCliente() {
-    const respuesta = await fetch(URL);
-    const datosCliente = await respuesta.json();
-    setCliente(datosCliente);
-    console.log(datosCliente);
-  }
-
-  useEffect(() => {
-    obtenerDatosCliente();
-  }, []);
-
-
-
+  
   return (
     <div className="p-5">
       <PmTableTravelAll />
