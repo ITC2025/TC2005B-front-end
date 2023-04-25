6e59cbcd1101285d9ce84ee700f759f0b562ded9
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-24 17:54:10 -0700

    Merge branch 'main' of https://github.com/ITC2025/TC2005B-front-end

--- src/utils/getApiData.js
+++ src/utils/getApiData.js
@@ -22,7 +22,7 @@ export function tokenValidation(){
     const data = sessionStorage.getItem("data")
     //Checa si no habia nada en los datos
     if (data === null) return -1
-    if (data.length === 2) return 1
+    if (data.length === 2) return -1
 
     const parsedJson=JSON.parse(data)
     const bToken=parsedJson.token
@@ -32,7 +32,16 @@ export function tokenValidation(){
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const decodedPayload = atob(base64);
     const payload = JSON.parse(decodedPayload);
-  
-    return(payload.rol)
 
+    if(Date.now() >= payload.exp * 1000) {
+        sessionStorage.clear()
+        return -1
+    }
+
+    return(payload.rol)
+}
+
+export function sessionDelete() {
+    sessionStorage.clear();
+    window.location.replace('/');
 }
