db69c163120e5a0fa3046f6fb05d7e7222e0bc47
MMMAAARRRTTT <mctulio.2@gmail.com>
2023-04-24 17:52:29 -0700

    fix: userDashboard page

--- src/pages/Dashboard/userDashboard.js
+++ src/pages/Dashboard/userDashboard.js
@@ -1,11 +1,29 @@
 import React from "react";
-import CardGridUser from "../../components/cards/CardGridUser";
+import CardRequest from "../../images/CardRequest.png";
+import CardTravelAll from "../../images/CardTravelAll.png";
+import CardDashboard from "../../components/cards/CardDashboard";
+
 
 export const UserDashboard = () => {
   return (
-    <CardGridUser
-      linkRequest="https://t4.ftcdn.net/jpg/02/36/57/91/360_F_236579161_BwYfCPuAytiZRCImG6yIe5RDBUlYpLCq.jpg"
-      linkTravelAll="https://mayaonmoney.co.za/wp-content/uploads/2021/03/travel-allowance.jpg"
-    />
+    <div className="container p-5">
+     <div className="container text-center">
+        <div className="my-3"></div>
+        <div className="row justify-content-center">
+        <div className="col-md-6">
+            <CardDashboard
+              image={CardTravelAll}
+              link={"https://mayaonmoney.co.za/wp-content/uploads/2021/03/travel-allowance.jpg"}
+            ></CardDashboard>
+          </div>
+          <div className="col-md-6">
+            <CardDashboard
+              image={CardRequest}
+              link={"https://t4.ftcdn.net/jpg/02/36/57/91/360_F_236579161_BwYfCPuAytiZRCImG6yIe5RDBUlYpLCq.jpg"}
+            ></CardDashboard>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
