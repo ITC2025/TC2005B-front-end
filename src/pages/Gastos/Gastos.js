dc687c89e86a1656c7637984bc2ff4c308485b3f
ElMeGaMerX <javierlolxd00537@gmail.com>
2023-04-27 16:35:57 -0700

    Form Data in one json

--- src/pages/Gastos/Gastos.js
+++ src/pages/Gastos/Gastos.js
@@ -47,7 +47,6 @@ export default function Facturas() {
                     <Col>
                         <Card id="container1">
                             <Container>
-
                                 <Gastos onAddInput={handleNewGastos} />
                             </Container>
                         </Card>
