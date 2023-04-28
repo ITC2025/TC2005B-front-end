2d8e280d357fd57e3faf44d2388e3f1474194570
m <m@tag.local>
2023-04-27 20:17:24 -0700

    Temporary fix for private routes

--- src/utils/PrivateRoutes.js
+++ src/utils/PrivateRoutes.js
@@ -1,13 +1,24 @@
 import {Outlet, Navigate} from 'react-router-dom'
 import { tokenValidation } from './getApiData'
+import { useEffect, useState } from 'react';
 
 const PrivateRoutes = ({rol}) => {
-    tokenValidation().then((andrecasmac)=>{
-        return(
-            andrecasmac.role===rol ? <Outlet/> : <Navigate to="/"/>
-        )
-    })
-    
+      const [has_permission, set_permission] = useState(true);
+
+      useEffect(() => {
+          const get_data = async () => {
+              const response = await tokenValidation();
+              set_permission(response.role);
+          };
+
+          get_data();
+      }, [has_permission]);
+
+    if (has_permission == rol) {
+        return <Outlet/>;
+    } else {
+        return <Navigate to="/" />;
+    }
 }
 
 export default PrivateRoutes
