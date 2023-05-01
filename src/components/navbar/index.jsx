f5b4be884126bbfe3f29846863a1e12bac3ad358
Jordan <a01252477@itesm.mx>
2023-05-01 16:45:52 -0700

    modificaciones minimas

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,8 +4,6 @@ import '../../styles/navbar.css'
 import { Outlet } from "react-router-dom";
 import { sessionDelete } from "../../apis/getApiData";
 import { useNavigate } from 'react-router-dom';
-import { sessionDelete } from "../../apis/getApiData";
-import { useNavigate } from 'react-router-dom';
 
 const NavbarSC = ({ client, projectManager, admin }) => {
 
@@ -29,15 +27,6 @@ const NavbarSC = ({ client, projectManager, admin }) => {
 
     // pm
     const pmProyectos = () => {
-<<<<<<< HEAD
-        navigate('/pm/');
-    }
-    const pmSolicitud = () => {
-        navigate('/pm/');
-    }
-    const pmHistorial = () => {
-        navigate('/pm/');
-=======
         navigate('/pm/proyectos');
     }
     const pmSolicitud = () => {
@@ -45,7 +34,6 @@ const NavbarSC = ({ client, projectManager, admin }) => {
     }
     const pmHistorial = () => {
         navigate('/pm/historico');
->>>>>>> origin
     }
 
     // admin
