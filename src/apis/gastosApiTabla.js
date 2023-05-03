3059d8a754d240dfb4f1714aa409a1bbf3a18790
Jaza2401 <alex.jauregui.zarate@gmail.com>
2023-05-03 11:11:09 -0700

    - aceptacion modal

--- src/apis/gastosApiTabla.js
+++ src/apis/gastosApiTabla.js
@@ -169,7 +169,22 @@ export async function send_expenses(id){
   return response;
 }
 
-
+export async function accept_viatico(id){
+  const url = 'http://localhost:3001/viatico_request/' + JSON.stringify(id);
+  console.log(url);
+  const options = {
+      method: "PATCH",
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ID_status_solicitud_viaticos: 3
+      }),
+  }
+  const rawResponse = await fetch(url, options)
+  const response = await rawResponse.json();
+  return response;
+}
 
 
 
