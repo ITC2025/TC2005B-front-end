440455e5dbf383f1feb1f1686736941b82cdf93d
mazomenos31 <gustbetancourt@hotmail.com>
2023-05-02 18:11:47 -0700

    changed variable for table to send correct value

--- src/components/table/TableTravelAllowance.jsx
+++ src/components/table/TableTravelAllowance.jsx
@@ -90,7 +90,7 @@ export const TableTravelAllowance = () => {
     },
     {
       name: "Actions",
-      cell: (row) => <TableDropdown viaticoID={row.ID_solicitud_viatico} />,
+      cell: (row) => <TableDropdown viaticoID={row.ID} />,
       width: "80px",
       style: { paddingLeft: "0.5em" },
     },
