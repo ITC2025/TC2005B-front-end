d41e952d2e9f93667f9d615e59cc8ef8e4f5a3d6
Kevin Valdez <kevinalexisvaldezz@hotmail.com>
2023-05-04 12:42:53 -0700

    Update: Remove unnecessary import for navigate

--- src/pages/SolicitarViaticos/SolicitarViaticos.js
+++ src/pages/SolicitarViaticos/SolicitarViaticos.js
@@ -5,11 +5,8 @@ import AddInputButton from "../../components/SolicitarViaticos/AddInputButton";
 import RequestModal from "../../components/SolicitarViaticos/RequestModal";
 import "../../styles/SolicitarViaticos.css";
 import { postEstimatedExpenses, submitSV } from "../../utils/PostExpenses";
-import { useNavigate } from "react-router-dom";
 
 function SolicitarViaticos() {
-  const navigate = useNavigate();
-
   const pageRefresher = () => {
     window.location.reload(); // cambiar ruta
   };
