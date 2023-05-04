45bcd215a9601069181f9da4f73616017922f623
Ximena <ximenaooflopez@gmail.com>
2023-05-03 20:19:25 -0700

    Fix line

--- src/pages/Viaticos/adminViaticos.js
+++ src/pages/Viaticos/adminViaticos.js
@@ -1,5 +1,4 @@
 import React from "react";
-import TableAdminDropdown from "../../components/table/TableAdminDropdown";
 import AdminTableTravelAllRequest from "../../components/table/AdminTableTravelAllRequest";
 
 export default function AdminViaticos() {
