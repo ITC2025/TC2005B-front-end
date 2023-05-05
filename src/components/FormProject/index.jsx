485d68d9ff77849812761ee457c042c9c77e8907
pabloamtzs <pabloamtz00@hotmail.com>
2023-05-04 19:42:13 -0700

    merge-changes

--- src/components/FormProject/index.jsx
+++ src/components/FormProject/index.jsx
@@ -46,9 +46,8 @@ export default function FormProject({ PmData }) {
 
   const handleInputChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
-    const handleInputChange = (event) => {
-      setFormData({ ...formData, [event.target.name]: event.target.value });
-    }
+
+    };
 
   // function that if an error occurs, it will show the error modal
   const errorModal = () => {
@@ -63,82 +62,6 @@ export default function FormProject({ PmData }) {
 
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(false);
-
-    return (
-      <>
-        <Container className="d-flex justify-content-start">
-          <h3>Nuevo Proyecto</h3>
-        </Container>
-        <Container className="justify-content-center">
-          <hr />
-          <Form onSubmit={handleForm}>
-            <Form.Group className="m-3" controlId="formBasicUp">
-              <Row>
-                <Col>
-                  <Form.Label className="text-center">Nombre</Form.Label>
-
-                  <InputGroup className="mb-3">
-                    <Form.Control name="nombre" value={formData.nombre} type="text" onChange={handleInputChange} required />
-                  </InputGroup>
-                </Col>
-                <Col>
-                  <Form.Label className="text-left">Código</Form.Label>
-                  <InputGroup className="mb-3">
-                    <Form.Control name="codigoProyecto" value={formData.codigoProyecto} type="text" onChange={handleInputChange} required />
-                  </InputGroup>
-                </Col>
-              </Row>
-            </Form.Group>
-
-            <Form.Group className="m-3" controlId="formBasicDown" id="group2">
-              <Row>
-                <Col>
-                  <Form.Label>Descripción</Form.Label>
-                  <textarea
-                    class="form-control"
-                    id="exampleFormControlTextarea2"
-                    rows="6"
-                    name="descripcion"
-                    value={formData.descripcion}
-                    onChange={handleInputChange}
-                    required
-                  ></textarea>
-                </Col>
-              </Row>
-            </Form.Group>
-            <Container className="d-flex justify-content-end">
-              <Button type="submit" onClick={() => modalCambiarProyectoCreado(!modalProyectoCreado)} variant="primary"> Crear Proyecto </Button>
-            </Container>
-          </Form>
-        </Container>
-        <Container className="fixed-bottom">
-          <hr />
-        </Container>
-  
-      {/* Modales */}
-      {success && (
-          < Modal estado={modalProyectoCreado}
-            cambiarEstado={modalCambiarProyectoCreado}
-            msg={'PROYECTO CREADO'}
-            oneButton={true}
-            proyectoCreado={true}
-            onClosed={handleCloseSuccess}
-          />
-      )}
-
-      {error && (
-          < Modal estado={modalError}
-            cambiarEstado={modalCambiarError}
-            msg={'OCURRIÓ UN ERROR'}
-            oneButton={true}
-            onClosed={handleCloseError}
-          />
-      )}
-
-
-    </>
-    );
-  }
 
   return (
     <>
@@ -182,12 +105,33 @@ export default function FormProject({ PmData }) {
               </Col>
             </Row>
           </Form.Group>
-          {pathname !== "/admin/proyectos" && <Button type="submit">Submit</Button>}
+          {pathname !== "/admin/proyectos" && <Button type="submit" onClick={() => modalCambiarProyectoCreado(!modalProyectoCreado)} variant="primary">Submit</Button>}
         </Form>
       </Container>
       <Container className="fixed-bottom">
         <hr />
       </Container>
+
+      {/* Modales */}
+      {success && (
+          < Modal estado={modalProyectoCreado}
+            cambiarEstado={modalCambiarProyectoCreado}
+            msg={'PROYECTO CREADO'}
+            oneButton={true}
+            proyectoCreado={true}
+            onClosed={handleCloseSuccess}
+          />
+      )}
+
+      {error && (
+          < Modal estado={modalError}
+            cambiarEstado={modalCambiarError}
+            msg={'OCURRIÓ UN ERROR'}
+            oneButton={true}
+            onClosed={handleCloseError}
+          />
+      )}
     </>
   );
 }
+
